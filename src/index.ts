b13bf7a02367edab62bc3df386efb3a58570cb04
Joshua Burt <joshburt@shapeandshare.com>
2022-09-13 10:31:20 -0700

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
 /**
  * Exports
  **/
+
+/** Export Commands */
+export { AbstractCommand } from './commands/AbstractCommand'
+export { AuthenticateUserCommand } from './commands/AuthenticateUserCommand'
+
+/** Export Utilities */
+export { demandEnvVar } from './common/utils/EnvironmentUtills'
+
+/** Export Errors */
+export { RequestFailureError } from './errors/RequestFailureError'
+export { ExceededRetryCountError } from './errors/ExceededRetryCountError'
+export { UnkownRequestVerb } from './errors/UnkownRequestVerb'
+
+/** Export Types */
+export { AuthenticateUserRequest } from './types/AuthenticateUserRequest'
+export { RequestHeaders } from './types/RequestHeaders'
+export { RequestStatusCodes } from './types/RequestStatusCodes'
+export { RequestVerbType } from './types/RequestVerbType'
+export { RetryOptions } from './types/RetryOptions'
+export { Token } from './types/Token'
+export { TokenClaims } from './types/TokenClaims'
+export { WrappedRequest } from './types/WrappedRequest'
+
+/** Export Client */
+export { RowanTreeAuthServiceClient } from './RowanTreeAuthServiceClient'
