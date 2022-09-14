78448e494b70bad44ae99387cafe6b46cf70a6a8
Joshua Burt <joshburt@shapeandshare.com>
2022-09-14 11:25:16 -0700

    jwt decode functionality

--- src/RowanTreeAuthServiceClient.ts
+++ src/RowanTreeAuthServiceClient.ts
@@ -5,6 +5,8 @@ import { UserAuthenticateRequest } from './types/UserAuthenticateRequest'
 import { UserRegisterCommand } from './commands/UserRegisterCommand'
 import { User } from './types/User'
 import { UserRegisterRequest } from './types/UserRegisterRequest'
+import { TokenClaims } from './types/TokenClaims'
+import jwtDecode from 'jwt-decode'
 
 export class RowanTreeAuthServiceClient {
   readonly #userAuthCommand: UserAuthenticateCommand
@@ -23,5 +25,9 @@ export class RowanTreeAuthServiceClient {
   public async registerUser (userName: string, password: string, email: string): Promise<User> {
     const request: UserRegisterRequest = { password, username: userName, email }
     return await this.#userRegisterCommand.register(request)
+  }
+
+  public decodeJwt (jwt: string): TokenClaims {
+    return jwtDecode<TokenClaims>(jwt)
   }
 }
